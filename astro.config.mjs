c362740f6b38419d21e4037dd98cc9cf27fbcc89
Ahmoudenn <Ahmouden@proton.me>
2025-02-20 03:13:24 +0100

    changed site key to "www.ahmouden.com"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ahmouden.com",
+  site: "https://www.ahmouden.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
